878865ddd7170e460bb152ef55b2fab637fa2a65
hemstrow <whemstrom@ucdavis.edu>
2018-02-14 16:49:58 -0800

    alignments, tophat out ignored, and improved job scheduling scripts

---
+++ de_Bekker_2015/cufflink_scripts/SRR1986854_1.fastq.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+#SBATCH -t 3-12:00:00
+#SBATCH -n 4
+#SBATCH --mem 8G
+
+
+tophat -p 4 -G /home/smmoenga/RNASeq/ECL_Zombie/data/GCA_001272575.2_ASM127257v2_genomic.gff -o SRR1986854_1.fastq_top genome/GCA_001272575.2_ASM127257v2_genomic /home/smmoenga/RNASeq/ECL_Zombie/data/SRR1986854_1.fastq /home/smmoenga/RNASeq/ECL_Zombie/data/SRR1986854_2.fastq
+cufflinks -p 4 -o SRR1986854_1.fastq_cuff SRR1986854_1.fastq_top/accepted_hits.bam
+
