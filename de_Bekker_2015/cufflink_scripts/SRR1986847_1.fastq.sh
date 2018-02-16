7f6f3befd8d79c481bbb4a72f473fe04dbe5bcb0
hemstrow <whemstrom@ucdavis.edu>
2018-02-16 10:51:56 -0800

    more shit moving

--- de_Bekker_2015/cufflink_scripts/SRR1986847_1.fastq.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH -t 3-12:00:00
-#SBATCH -n 4
-#SBATCH --mem 8G
-
-
-tophat -p 4 -G /home/smmoenga/RNASeq/ECL_Zombie/data/GCA_001272575.2_ASM127257v2_genomic.gff -o SRR1986847_1.fastq_top genome/GCA_001272575.2_ASM127257v2_genomic /home/smmoenga/RNASeq/ECL_Zombie/data/SRR1986847_1.fastq /home/smmoenga/RNASeq/ECL_Zombie/data/SRR1986847_2.fastq
-cufflinks -p 4 -o SRR1986847_1.fastq_cuff SRR1986847_1.fastq_top/accepted_hits.bam
-
